a63d552416c93846523ae70744fbb303ddb41697
iagoLopex <105664489+iagoLopex@users.noreply.github.com>
2022-10-05 13:28:38 -0300

    Update and rename 147test.cpp to 147.cpp

--- algorithms/Backtracking/147.cpp
+++ algorithms/Backtracking/147.cpp
@@ -4,9 +4,9 @@ using namespace std;
 long double n;
 
 int coin[11]={10000, 5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5};
-long long int dp[30010][12];
+long long int dp[40010][12];
 
-int func(int valor, int i){
+long long int func(int valor, int i){
 	
 	if(valor == 0) return 1;
 	if(i > 10) return 0;
@@ -14,7 +14,6 @@ int func(int valor, int i){
 	
 	long long int take = 0;
 	if((valor-coin[i])>=0) take = func( valor-coin[i], i );
-	
 	long long int Dtake = func( valor, i+1 );
 	
 	return dp[valor][i] = (take + Dtake);
@@ -23,13 +22,15 @@ int func(int valor, int i){
 int main(){
 
 	cin >> n;
-	while(n != 0.0){
+	long double k;
+	while(n != 0){
+		k=n;
 		memset(dp, -1, sizeof dp);
 		n*=10000;
 		n/=100;
 		
 		cout << fixed << setprecision(2);
-		cout << setw(6) << setfill(' ') << n;
+		cout << setw(6) << setfill(' ') << k;
 		cout << setw(17) << setfill(' ') << func(n, 0) << endl;
 		cin >> n;
 	}
