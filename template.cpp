02d47e45192589d5709135c8141b9f3d471275ae
iagoLopex <105664489+iagoLopex@users.noreply.github.com>
2023-04-13 19:14:58 -0300

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -11,8 +11,10 @@ using namespace std;
 #define fin  freopen("int", "r", stdin)
 #define fout freopen("out", "w", stdout)
 #define FORr(vec) for(int i = 0; i < sz(vec); i++) cin >> vec[i];
-#define FORw(vec) for(int i = 0; i < sz(vec); i++) cout << vec[i] << " \n"[i==sz(vec)];
+#define FORw(vec) for(int i = 0; i < sz(vec); i++) cout << vec[i] << " \n"[i==sz(vec)-1];
 #define fast ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+/*-------------------------------------------------------------------------------------------*/
 
 signed main(){
 	
