19b004fd7880efc794f72c873f92bc0d18794565
iagoLopex <105664489+iagoLopex@users.noreply.github.com>
2023-06-19 13:32:32 -0300

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -8,7 +8,7 @@ using namespace std;
 #define pb push_back
 #define int long long
 #define sz(vec) (int)vec.size()
-#define fin  freopen("int", "r", stdin)
+#define fin  freopen("in", "r", stdin)
 #define fout freopen("out", "w", stdout)
 #define FORr(vec) for(int i = 0; i < sz(vec); i++) cin >> vec[i];
 #define FORw(vec) for(int i = 0; i < sz(vec); i++) cout << vec[i] << " \n"[i==sz(vec)-1];
